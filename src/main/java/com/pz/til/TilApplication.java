bd940ee615cb057d6a36e8a50f4328de64a3ae90
pzlotowski <piotrekzlotowski@gmail.com>
2017-07-09 19:09:47 +0200

    Initial commit. Project structure

---
+++ src/main/java/com/pz/til/TilApplication.java
@@ -0,0 +1,13 @@
+package com.pz.til;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class TilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilApplication.class, args);
+	}
+}
